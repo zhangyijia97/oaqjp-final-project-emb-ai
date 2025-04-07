f72f852010a140f5d6dc16bac216d18ccaa7c65c
zhangyijia97 <zhangyijia1997@gmail.com>
2025-04-07 03:18:31 -0400

    emotion_detector() now returns the value of each emotion along with the dominant emotion in a dictionary format

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,21 @@ def emotion_detector(text_to_analyse):
     myobj =  { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json=myobj, headers=header)
-    return json.loads(response.text)
+
+    formatted_response = formatted_response = json.loads(response.text)
+
+    output = {
+        'anger': formatted_response["emotionPredictions"][0]["emotion"]["anger"],
+        'disgust': formatted_response["emotionPredictions"][0]["emotion"]["disgust"],
+        'fear': formatted_response["emotionPredictions"][0]["emotion"]["fear"],
+        'joy': formatted_response["emotionPredictions"][0]["emotion"]["joy"],
+        'sadness': formatted_response["emotionPredictions"][0]["emotion"]["sadness"],
+        'dominant_emotion': None
+    }
+    max_value = 0
+    for key in ['anger', 'disgust', 'fear', 'joy', 'sadness']:
+        if output[key] > max_value:
+            max_value = output[key]
+            highest_emotion = key.capitalize()
+    output['dominant_emotion'] = highest_emotion
+    return output
