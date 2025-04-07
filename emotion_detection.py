260bb9e6a9e69d55cdd94c9d2c5d582438a14c3e
zhangyijia97 <zhangyijia1997@gmail.com>
2025-04-07 02:28:27 -0400

    Implemented emotion detector

---
+++ emotion_detection.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url =  'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header =  {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj =  { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url, json=myobj, headers=header)
+    return json.loads(response.text)
