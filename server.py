18560e787d484e753cddc2bf180ca56ed2082978
zhangyijia97 <zhangyijia1997@gmail.com>
2025-04-07 04:01:51 -0400

    Implemented emotion detection function in server.py

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    return (f"For the given statement, the system response is "
+    f"'anger': {anger}, "
+    f"'disgust': {disgust}, "
+    f"'fear': {fear}, "
+    f"'joy': {joy} and "
+    f"'sadness': {sadness}. "
+    f"The dominant emotion is {dominant_emotion}.")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
